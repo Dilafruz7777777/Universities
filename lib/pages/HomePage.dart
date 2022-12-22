8c471a33401b867be9dc360f5289ab53a1d72971
Asamutdinova_Dilafruz <dilafruzasamutdinovaflutter@gmail.com>
2022-12-22 22:17:27 +0500

    Telefon yoki smsni, web brauzerni tanlashni chiqarish original varianti

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart' as url_launcher;
+import 'package:url_launcher/url_launcher.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -13,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   
   dynamic data;
   bool isLoading = true;
-  
+
   Future<void> getInformation() async {
     isLoading = true;
     setState(() {});
@@ -50,10 +51,41 @@ class _HomePageState extends State<HomePage> {
                 borderRadius: BorderRadius.all(Radius.circular(32),),
                 color: Colors.blueAccent.withOpacity(0.5),
               ),
-                child: Text(data[index]["name"],
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
+                child: Column(
+                  children: [
+                    Text(data[index]["name"],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+
+                    SizedBox(height: 32),
+                    TextButton(onPressed: () async {
+                      final  launcherUri= Uri.parse(data[index]["web_pages"] ? [0] ?? "");
+                      await url_launcher.launchUrl(
+                        launcherUri,
+                        mode: LaunchMode.platformDefault,
+                      );
+                    },
+                        child: Text(data[index]["web_pages"]?[0] ?? "",
+                          style: TextStyle(color: Colors.white),
+                        )),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final Uri launchUri =
+                            Uri(scheme: "sms", path: "+998901166563");
+                        await url_launcher.launchUrl(launchUri);
+                      }, child: Text("number",
+                      style: TextStyle(color: Colors.white),
+                    )
+                    )
+
+                  ],
                 ),
+
+
             );
           },
         ));
